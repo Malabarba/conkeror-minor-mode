e2de23647b126d88d74920a52729371782247af1
Artur Malabarba <bruce.connor.am@gmail.com>
2013-10-25 20:03:14 +0100

    Font-locking

--- conkeror-minor-mode.el
+++ conkeror-minor-mode.el
@@ -4,7 +4,7 @@
 
 ;; Author: Artur Malabarba <bruce.connor.am@gmail.com>>
 ;; URL: http://github.com/BruceConnor/conkeror-minor-mode
-;; Version: 1.0
+;; Version: 1.3
 ;; Keywords: programming tools
 ;; Prefix: conkeror
 ;; Separator: -
@@ -63,11 +63,12 @@
 ;; 
 
 ;;; Change Log:
+;; 1.3 - 20131025 - Font-locking
 ;; 1.0 - 20131025 - Created File.
 ;;; Code:
 
-(defconst conkeror-minor-mode-version "1.0" "Version of the conkeror-minor-mode.el package.")
-(defconst conkeror-minor-mode-version-int 1 "Version of the conkeror-minor-mode.el package, as an integer.")
+(defconst conkeror-minor-mode-version "1.3" "Version of the conkeror-minor-mode.el package.")
+(defconst conkeror-minor-mode-version-int 2 "Version of the conkeror-minor-mode.el package, as an integer.")
 (defun conkeror-bug-report ()
   "Opens github issues page in a web browser. Please send me any bugs you find, and please inclue your emacs and conkeror versions."
   (interactive)
@@ -159,10 +160,49 @@ with single quotes in linux command shells."
           (forward-sexp 1)))
       (buffer-substring-no-properties l r))))
 
+(defconst conkeror--font-lock-keywords
+  '(;; keywords
+    ("\\_<\\(\\$\\(?:a\\(?:ction\\|l\\(?:ign\\|low_www\\|ternative\\)\\|nonymous\\|rgument\\|uto\\(?:_complete\\(?:_\\(?:delay\\|initial\\)\\)?\\)?\\)\\|b\\(?:inding\\(?:_list\\|s\\)?\\|rowser_object\\|uffers?\\)\\|c\\(?:harset\\|lass\\|o\\(?:m\\(?:mand\\(?:_list\\)?\\|plet\\(?:er\\|ions\\)\\)\\|nstructor\\)\\|rop\\|wd\\)\\|d\\(?:e\\(?:fault\\(?:_completion\\)?\\|scription\\)\\|isplay_name\\|o\\(?:c\\|mains?\\)\\)\\|f\\(?:allthrough\\|ds\\|lex\\)\\|get_\\(?:description\\|string\\)\\|h\\(?:e\\(?:aders\\|lp\\)\\|i\\(?:nt\\(?:_xpath_expression\\)?\\|story\\)\\)\\|in\\(?:dex_file\\|fo\\|itial_value\\)\\|key\\(?:_sequence\\|map\\)\\|load\\|m\\(?:atch_required\\|od\\(?:ality\\|e\\)\\|ultiple\\)\\|name\\(?:space\\)?\\|o\\(?:bject\\|p\\(?:ener\\|ml_file\\|tions\\)\\|ther_bindings\\|verride_mime_type\\)\\|p\\(?:a\\(?:rent\\|ssword\\|th\\)\\|erms\\|osition\\|r\\(?:e\\(?:fix\\|pare_download\\)\\|ompt\\)\\)\\|re\\(?:gexps\\|peat\\)\\|s\\(?:elect\\|hell_command\\(?:_cwd\\)?\\)\\|t\\(?:e\\(?:mp_file\\|st\\)\\|lds\\)\\|u\\(?:rl\\(?:\\(?:_prefixe\\)?s\\)\\|se\\(?:_\\(?:bookmarks\\|cache\\|history\\|webjumps\\)\\|r\\)\\)\\|va\\(?:lidator\\|riable\\)\\|wrap_column\\)\\)\\_>"
+     1 font-lock-constant-face)
+    ;; Major functions
+    ("\\_<\\(\\(?:interactiv\\|requir\\)e\\)\\_>\\s-*("
+     1 font-lock-keyword-face)
+    ;; common functions
+    ("\\(a\\(?:dd_hook\\|lternates\\)\\|build_url_regexp\\|call_on_focused_field\\|define_\\(?:browser_object_class\\|key\\(?:map\\(?:s_page_mode\\)?\\)?\\|webjump\\)\\|exec\\|focus_next\\|mod\\(?:e_line_\\(?:adder\\|mode\\)\\|ify_region\\)\\|p\\(?:age_mode_activate\\|op\\|ush\\)\\|re\\(?:ad_from_clipboard\\|gister_user_stylesheet\\|move_hook\\)\\|s\\(?:e\\(?:ssion_pref\\|t_protocol_handler\\)\\|witch_to_buffer\\)\\|test\\)\\s-*("
+     1 font-lock-function-name-face)
+    ;; keymaps
+    ("\\_<\\(\\(?:c\\(?:aret\\|ontent_buffer_\\(?:anchor\\|button\\|checkbox\\|embed\\|form\\|normal\\|richedit\\|select\\|text\\(?:area\\)?\\)\\)\\|d\\(?:ef\\(?:ault_\\(?:base\\|global\\|help\\)\\|ine\\)\\|ownload_buffer\\|uckduckgo\\(?:_\\(?:anchor\\|select\\)\\)?\\)\\|f\\(?:acebook\\|eedly\\|ormfill\\)\\|g\\(?:ithub\\|lobal_overlay\\|ma\\(?:il\\|ne\\)\\|oogle_\\(?:calendar\\|gqueues\\|maps\\|reader\\|search_results\\|voice\\)\\|rooveshark\\)\\|h\\(?:elp_buffer\\|int\\(?:_quote_next\\)?\\)\\|isearch\\|key_binding_reader\\|list_by\\|minibuffer\\(?:_\\(?:base\\|message\\|space_completion\\)\\)?\\|new\\(?:sblur\\)?\\|over\\(?:lay\\|ride\\)\\|quote\\(?:_next\\)?\\|re\\(?:ad_buffer\\|ddit\\)\\|s\\(?:equence_\\(?:abort\\|help\\)\\|ingle_character_options_minibuffer\\|pecial_buffer\\|tackexchange\\)\\|t\\(?:arget\\|ext\\|witter\\)\\|universal_argument\\|wikipedia\\|youtube_player\\)_keymap\\)\\_>"
+     1 font-lock-variable-name-face)
+    ;; mods
+    ("\\_<\\(\\(?:d\\(?:ailymotion\\|uckduckgo\\)\\|f\\(?:acebook\\|eedly\\)\\|g\\(?:ithub\\|ma\\(?:il\\|ne\\)\\|oogle_\\(?:calendar\\|gqueues\\|images\\|maps\\|reader\\|search_results\\|v\\(?:ideo\\|oice\\)\\)\\|rooveshark\\)\\|key_kill\\|newsblur\\|reddit\\|s\\(?:mbc\\|tackexchange\\)\\|twitter\\|wikipedia\\|xkcd\\|youtube\\(?:_player\\)?\\)_mode\\)\\_>"
+     1 font-lock-variable-name-face)
+    ;; user variables
+    ("\\_<\\(a\\(?:ctive_\\(?:\\(?:img_\\)?hint_background_color\\)\\|llow_browser_window_close\\|uto_mode_list\\)\\|b\\(?:lock_content_focus_change_duration\\|rowser_\\(?:automatic_form_focus_window_duration\\|default_open_target\\|form_field_xpath_expression\\|relationship_patterns\\)\\|ury_buffer_position\\)\\|c\\(?:an_kill_last_buffer\\|l\\(?:icks_in_new_buffer_\\(?:button\\|target\\)\\|ock_time_format\\)\\|ontent_handlers\\|wd\\)\\|d\\(?:aemon_quit_exits\\|e\\(?:fault_minibuffer_auto_complete_delay\\|lete_temporary_files_for_command\\)\\|ownload_\\(?:buffer_\\(?:automatic_open_target\\|min_update_interval\\)\\|temporary_file_open_buffer_delay\\)\\)\\|e\\(?:dit\\(?:_field_in_external_editor_extension\\|or_shell_command\\)\\|xternal_\\(?:\\(?:content_handler\\|editor_extension_override\\)s\\)\\|ye_guide_\\(?:context_size\\|highlight_new\\|interval\\)\\)\\|f\\(?:avicon_image_max_size\\|orced_charset_list\\)\\|generate_filename_safely_fn\\|h\\(?:int\\(?:_\\(?:background_color\\|digits\\)\\|s_a\\(?:\\(?:mbiguous_a\\)?uto_exit_delay\\)\\)\\|omepage\\)\\|i\\(?:mg_hint_background_color\\|ndex_\\(?:webjumps_directory\\|xpath_webjump_tidy_command\\)\\|search_keep_selection\\)\\|k\\(?:ey\\(?:_bindings_ignore_capslock\\|board_key_sequence_help_timeout\\)\\|ill_whole_line\\)\\|load_paths\\|m\\(?:edia_scrape\\(?:_default_regexp\\|rs\\)\\|i\\(?:me_type_external_handlers\\|nibuffer_\\(?:auto_complete_\\(?:default\\|preferences\\)\\|completion_rows\\|history_max_items\\|input_mode_show_message_timeout\\|read_url_select_initial\\)\\)\\)\\|new_buffer_\\(?:\\(?:with_opener_\\)?position\\)\\|opensearch_load_paths\\|r\\(?:ead_\\(?:buffer_show_icons\\|url_handler_list\\)\\|un_external_editor_function\\)\\|title_format_fn\\|url_\\(?:completion_\\(?:sort_order\\|use_\\(?:bookmarks\\|history\\|webjumps\\)\\)\\|remoting_fn\\)\\|view_source_\\(?:function\\|use_external_editor\\)\\|w\\(?:ebjump_partial_match\\|indow_extra_argument_max_delay\\)\\|xkcd_add_title\\)\\_>"
+     1 font-lock-variable-name-face)))
+
 ;;;###autoload
 (define-minor-mode conkeror-minor-mode nil nil " Conk"
   '(("" . eval-in-conkeror))
-  :group 'conkeror-minor-mode)
+  :group 'conkeror-minor-mode
+  (if conkeror-minor-mode  ;(regexp-opt '())
+      (font-lock-add-keywords
+       nil
+       conkeror--font-lock-keywords)))
+
 
 (provide 'conkeror-minor-mode)
 ;;; conkeror-minor-mode.el ends here.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
